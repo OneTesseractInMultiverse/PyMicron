375694c93c61723a8f070665027575c5f0a380c3
Pedro Guzmán <OneTesseractInMultiverse@users.noreply.github.com>
2018-10-24 05:44:55 -0600

    Update env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 #!/usr/bin/env bash
 
-export SECRET_KEY=Awsx1Sedc2Drfv34
-export LOG_FILE=ml_authorization_xs.log
+export SECRET_KEY=[your_secret_key]
+export LOG_FILE=micron.log
 
-export JWT_SECRET_KEY=GnRDqw6EYXsqYfXp
+export JWT_SECRET_KEY=[your_jwt_signing_key]
 export JWT_PUBLIC_KEY=$(cat ../local/ec256-public.pem)
 export JWT_PRIVATE_KEY=$(cat ../local/ec256-private.pem)
 export JWT_ALGORITHM=ES256
 
-export CELERY_BROKER_URL=amqp://dqfdfigw:Ps3vBK7GdDWHxXz1YqR7f7R4tTSlJfhS@white-swan.rmq.cloudamqp.com/dqfdfigw
-export CELERY_RESULT_BACKEND=amqp://dqfdfigw:Ps3vBK7GdDWHxXz1YqR7f7R4tTSlJfhS@white-swan.rmq.cloudamqp.com/dqfdfigw
-export MESSAGING_QUEUE=amqp://dqfdfigw:Ps3vBK7GdDWHxXz1YqR7f7R4tTSlJfhS@white-swan.rmq.cloudamqp.com/dqfdfigw
+export CELERY_BROKER_URL=[rabbit_or_redis_connection_string]
+export CELERY_RESULT_BACKEND=[rabbit_or_redis_connection_string]
+export MESSAGING_QUEUE=[rabbit_or_redis_connection_string]
